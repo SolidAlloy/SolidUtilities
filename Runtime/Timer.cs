7d61f96ca8beafce856cb64f2a571f7f7af69217
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-06 22:58:44 +0300

    feat: Added a measure unit choice for Timer.LogTime()

--- Runtime/Timer.cs
+++ Runtime/Timer.cs
@@ -4,14 +4,19 @@
     using System.Diagnostics;
     using Debug = UnityEngine.Debug;
 
+    public enum TimeUnit { Milliseconds, Nanoseconds }
+
     /// <summary>
     /// Basic timer that logs execution time of a method or part of the method. It does not warm up the execution and
     /// runs the actions only once.
     /// </summary>
     public static class Timer
     {
+        private const int NanosecondsInAMillisecond = 1000000;
+
         /// <summary>Log time in ms the action took.</summary>
         /// <param name="actionName">Name of the action which execution is measured.</param>
+        /// <param name="timeUnit">The time unit to use (ms, ns, etc.)</param>
         /// <param name="action">Action to execute.</param>
         /// <example><code>
         /// LogTime("Show popup", () =>
@@ -20,12 +25,24 @@
         ///     dropdownWindow.ShowInPopup();
         /// });
         /// </code></example>
-        public static void LogTime(string actionName, Action action)
+        public static void LogTime(string actionName, TimeUnit timeUnit, Action action)
         {
             var stopWatch = Stopwatch.StartNew();
             action();
             stopWatch.Stop();
-            Debug.Log($"{actionName} took {Convert.ToInt32(stopWatch.ElapsedMilliseconds)} ms.");
+
+            switch (timeUnit)
+            {
+                case TimeUnit.Milliseconds:
+                    Debug.Log($"{actionName} took {Convert.ToInt32(stopWatch.ElapsedMilliseconds)} ms.");
+                    break;
+                case TimeUnit.Nanoseconds:
+                    Debug.Log($"{actionName} took {Convert.ToInt32(stopWatch.Elapsed.TotalMilliseconds * NanosecondsInAMillisecond)} ns.");
+                    break;
+                default:
+                    Debug.Log($"{nameof(LogTime)} does not have an implementation of the following time unit yet: {timeUnit}.");
+                    break;
+            }
         }
     }
 }
