0d238273e512c28e447ae68f544d38691a50a18c
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-14 19:49:21 +0300

    feat: Added Vector2.Center(Rect outerRect) method
    It create a smaller rect that is centered inside the bigger one

---
+++ Runtime/Extensions/Vector2Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SolidUtilities.Extensions
+{
+    using JetBrains.Annotations;
+    using UnityEngine;
+
+    /// <summary>Different useful extensions for <see cref="Vector2"/>.</summary>
+    public static class Vector2Extensions
+    {
+        [PublicAPI] public static Rect Center(this Vector2 smallerRectSize, Rect outerRect)
+        {
+            float horizontalPadding = (outerRect.width - smallerRectSize.x) / 2f;
+            float verticalPadding = (outerRect.height - smallerRectSize.y) / 2f;
+            var smallerRectPosition = new Vector2(outerRect.x + horizontalPadding, outerRect.y + verticalPadding);
+            return new Rect(smallerRectPosition, smallerRectSize);
+        }
+    }
+}
