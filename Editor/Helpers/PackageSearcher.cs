3e42cfe42334486087512f2d0b78cdcacb405b63
SolidAlloy <artem.perepelytsia@4a-games.com.mt>
2020-10-23 14:19:12 +0300

    wip: Added the PackageSearcher class

---
+++ Editor/Helpers/PackageSearcher.cs
@@ -0,0 +1,12 @@
+﻿namespace SolidUtilities.Editor.Helpers
+{
+    using JetBrains.Annotations;
+    using UnityEditor.PackageManager;
+
+    public static class PackageSearcher
+    {
+        [PublicAPI, CanBeNull]
+        public static PackageInfo FindPackageByName(string packageName) =>
+            PackageInfo.FindForAssetPath($"Packages/{packageName}");
+    }
+}
