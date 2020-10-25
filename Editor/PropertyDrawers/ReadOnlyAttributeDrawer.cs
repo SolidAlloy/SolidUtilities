7b5f46d26208696d7e55bb0c68c39536ad5eb4ea
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-25 21:19:01 +0200

    feat: Added the ReadOnly attribute

---
+++ Editor/PropertyDrawers/ReadOnlyAttributeDrawer.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericScriptableObjects.Usage_Example.Editor
+{
+    using SolidUtilities.Attributes;
+    using SolidUtilities.Helpers;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    internal class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            DrawHelper.WithDisabledGUI(() =>
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            });
+        }
+    }
+}
---
+++ Runtime/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace SolidUtilities.Attributes
+{
+    using UnityEngine;
+
+    /// <summary>Shows a field as disabled preventing it from being changed from the inspector.</summary>
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
