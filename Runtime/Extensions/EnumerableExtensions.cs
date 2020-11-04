34222872a9cbdf862bc6babf255b33acb6c12109
SolidAlloy <perepelartem@gmail.com>
2020-11-04 09:28:06 +0200

    feat: Added IEnumerable.ToHashSet() method

---
+++ Runtime/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace SolidUtilities.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Converts the collection to HashSet.
+        /// </summary>
+        /// <param name="source">The source collection.</param>
+        /// <typeparam name="T">Type of the collection items.</typeparam>
+        /// <returns>HashSet containing of the collection items.</returns>
+        /// <exception cref="ArgumentNullException">If source is null.</exception>
+        [PublicAPI, NotNull] public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source) => new HashSet<T>(source);
+    }
+}
