b25f81a3ac0d6d108d0bc533f456ff23f8f9639c
SolidAlloy <tadamori.yoshi@gmail.com>
2020-11-08 23:05:43 +0200

    feat: Made ChildProperties iterate over visible properies only by default

--- Editor/ChildProperties.cs
+++ Editor/ChildProperties.cs
@@ -11,7 +11,7 @@
     /// Allows iterating over child properties of a serialized object without entering nested properties.
     /// </summary>
     /// <example><code>
-    /// var childProperties = new ChildProperties(serializedObject, true);
+    /// var childProperties = new ChildProperties(serializedObject);
     /// foreach (var child in childProperties)
     /// {
     ///     FieldInfo field = targetType.GetFieldAtPath(child.propertyPath);
@@ -23,6 +23,8 @@
         private readonly SerializedObject _parentObject;
         private readonly bool _enterChildren;
         private readonly bool _excludeBuiltInProperties;
+        private readonly bool _visibleOnly;
+
         private SerializedProperty _currentProp;
         private bool _nextPropertyExists;
 
@@ -32,12 +34,13 @@
         /// <param name="parentObject">The parent serialized object which child properties you want to inspect.</param>
         /// <param name="enterChildren">Whether to iterate through child properties recursively. <c>false</c> by default.</param>
         /// <param name="excludeBuiltInProperties">Whether to exclude built-in properties from the iteration. <c>true</c> by default.</param>
-        public ChildProperties(SerializedObject parentObject, bool enterChildren = false, bool excludeBuiltInProperties = true)
+        /// <param name="visibleOnly">Whether to iterate only over the visible properties.</param>
+        public ChildProperties(SerializedObject parentObject, bool enterChildren = false, bool excludeBuiltInProperties = true, bool visibleOnly = true)
         {
             _parentObject = parentObject;
             _enterChildren = enterChildren;
             _excludeBuiltInProperties = excludeBuiltInProperties;
-            Reset();
+            _visibleOnly = visibleOnly;
         }
 
         SerializedProperty IEnumerator<SerializedProperty>.Current => _currentProp;
@@ -48,12 +51,12 @@
             if ( ! _nextPropertyExists)
                 return false;
 
-            _nextPropertyExists = _currentProp.Next(_enterChildren);
+            _nextPropertyExists = _currentProp.Next(_enterChildren, _visibleOnly);
 
             if (_excludeBuiltInProperties)
             {
                 while (_nextPropertyExists && _currentProp.IsBuiltIn())
-                    _nextPropertyExists = _currentProp.Next(_enterChildren);
+                    _nextPropertyExists = _currentProp.Next(_enterChildren, _visibleOnly);
             }
 
             return _nextPropertyExists;
@@ -67,8 +70,24 @@
 
         void IDisposable.Dispose() { }
 
-        IEnumerator<SerializedProperty> IEnumerable<SerializedProperty>.GetEnumerator() => this;
+        IEnumerator<SerializedProperty> IEnumerable<SerializedProperty>.GetEnumerator()
+        {
+            Reset();
+            return this;
+        }
 
-        IEnumerator IEnumerable.GetEnumerator() => this;
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            Reset();
+            return this;
+        }
+    }
+
+    internal static class PropertyExtensions
+    {
+        public static bool Next(this SerializedProperty prop, bool enterChildren, bool visible)
+        {
+            return visible ? prop.NextVisible(enterChildren) : prop.Next(enterChildren);
+        }
     }
 }
