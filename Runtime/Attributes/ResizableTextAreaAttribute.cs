23664a5382d6990ccc5c99aa7d9fa4ca9f455b1e
SolidAlloy <artem.perepelytsia@4a-games.com.mt>
2020-11-12 14:18:47 +0200

    feat: Added ResizableTextArea attribute
    It will behave similarly to NaughtyAttributes' one but will wrap lines automatically.

---
+++ Runtime/Attributes/ResizableTextAreaAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace SolidUtilities.Attributes
+{
+    using System;
+    using JetBrains.Annotations;
+    using UnityEngine;
+
+    [AttributeUsage(AttributeTargets.Field)]
+    [BaseTypeRequired(typeof(string))]
+    public class ResizableTextAreaAttribute : PropertyAttribute { }
+}
