94b4c02eb8a21627a77fa1687e659aef2b74d67f
SolidAlloy <artem.perepelytsia@4a-games.com.mt>
2020-11-13 13:51:13 +0200

    feat: Added the CreateEditor<T>() method

--- Editor/Helpers/EditorDrawHelper.cs
+++ Editor/Helpers/EditorDrawHelper.cs
@@ -5,6 +5,7 @@
     using SolidUtilities.Helpers;
     using UnityEditor;
     using UnityEngine;
+    using Object = UnityEngine.Object;
 
     /// <summary>Different useful methods that simplify <see cref="EditorGUILayout"/> API.</summary>
     public static class EditorDrawHelper
@@ -251,6 +252,17 @@
             EditorGUI.indentLevel = indentLevel;
             drawContent();
             EditorGUI.indentLevel = previousIndentLevel;
+        }
+
+        /// <summary>Creates an editor of type <typeparamref name="T"/> for <paramref name="targetObject"/>.</summary>
+        /// <param name="targetObject">Target object to create an editor for.</param>
+        /// <typeparam name="T">Type of the editor to create.</typeparam>
+        /// <returns>Editor of type <typeparamref name="T"/>.</returns>
+        [PublicAPI, Pure]
+        public static T CreateEditor<T>(Object targetObject)
+            where T : Editor
+        {
+            return (T) Editor.CreateEditor(targetObject, typeof(T));
         }
     }
 }
