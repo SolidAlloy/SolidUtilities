d2b501e039c9d0a95f9c206bf388b53949dc41e3
SolidAlloy <perepelartem@gmail.com>
2020-12-01 18:14:46 +0200

    feat: Added RegexExtensions class with its first method - Find()

---
+++ Runtime/Extensions/RegexExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace SolidUtilities.Extensions
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Different useful extension methods for <see cref="Regex"/>.
+    /// </summary>
+    public static class RegexExtensions
+    {
+        /// <summary>
+        /// Searches for a match in <paramref name="text"/> and returns it. If there is no match, throws <see cref="ArgumentException"/>.
+        /// </summary>
+        /// <param name="regex">The regex to use for search.</param>
+        /// <param name="text">The text to search in.</param>
+        /// <returns>Match that was found in the <paramref name="text"/>.</returns>
+        /// <exception cref="ArgumentException">If no match was found in <paramref name="text"/>.</exception>
+        [PublicAPI]
+        public static string Find(this Regex regex, string text)
+        {
+            string result = regex.Match(text).Value;
+
+            if (result == string.Empty)
+                throw new ArgumentException($"Failed to find \"{regex}\" in the following text: \"{text}\".");
+
+            return result;
+        }
+    }
+}
