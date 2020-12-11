419c150c27dd0304f5f53fc26f77cf2656dc6105
SolidAlloy <perepelartem@gmail.com>
2020-12-11 12:05:53 +0200

    Added ForEach<T>() LINQ extension method

---
+++ Runtime/Extensions/LinqExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace SolidUtilities.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+
+    /// <summary>Different useful LINQ extensions.</summary>
+    public static class LinqExtensions
+    {
+        /// <summary>Performs an action on each item.</summary>
+        /// <param name="source">The source to enumerate.</param>
+        /// <param name="action">The action to perform.</param>
+        [PublicAPI]
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T obj in source)
+                action(obj);
+
+            return source;
+        }
+    }
+}
