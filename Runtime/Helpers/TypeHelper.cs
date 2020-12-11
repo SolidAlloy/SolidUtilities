a89fbbf936081ecdd3cdc84a41b39940b9f04ad8
SolidAlloy <perepelartem@gmail.com>
2020-12-11 12:11:31 +0200

    feat: Added string.ReplaceWithBuiltInName() extension method for type names.

--- Runtime/Helpers/TypeHelper.cs
+++ Runtime/Helpers/TypeHelper.cs
@@ -1,11 +1,31 @@
 ﻿namespace SolidUtilities.Helpers
 {
     using System;
+    using System.Collections.Generic;
     using JetBrains.Annotations;
 
     /// <summary>Different helper functions for <see cref="System.Type"/></summary>
     public static class TypeHelper
     {
+        private static readonly Dictionary<string, string> BuiltInTypes = new Dictionary<string, string>
+        {
+            { "System.Boolean", "bool" },
+            { "System.Byte", "byte" },
+            { "System.SByte", "sbyte" },
+            { "System.Char", "char" },
+            { "System.Decimal", "decimal" },
+            { "System.Double", "double" },
+            { "System.Single", "float" },
+            { "System.Int32", "int" },
+            { "System.UInt32", "uint" },
+            { "System.Int64", "long" },
+            { "System.UInt64", "ulong" },
+            { "System.Int16", "short" },
+            { "System.UInt16", "ushort" },
+            { "System.Object", "object" },
+            { "System.String", "string" }
+        };
+
         /// <summary>Makes the type a generic type definition if it is not.</summary>
         /// <param name="typeToCheck">The type to get generic type definition from.</param>
         /// <returns>A type that was made GenericTypeDefinition.</returns>
@@ -55,6 +75,30 @@
             return char.IsDigit(typeName[separatorIndex + 1])
                 ? typeName.Substring(0, separatorIndex)
                 : typeName;
+        }
+
+        /// <summary>
+        /// Returns a built-in name for <paramref name="fullTypeName"/> if the built-in analogue exists.
+        /// </summary>
+        /// <param name="fullTypeName">Full name of the type.</param>
+        /// <param name="withFolder">Whether to prepend the built-in type with "Built-in.".</param>
+        /// <returns>A built-in name for <paramref name="fullTypeName"/> if the built-in analogue exists.</returns>
+        /// <example><code>
+        /// string intName = typeof(System.Int32).FullName;
+        /// Debug.Log(intName.TryReplaceWithBuiltInName()); // prints "Built-in.int"
+        ///
+        /// string intName = typeof(System.Int32).FullName;
+        /// Debug.Log(intName.TryReplaceWithBuiltInName(true)); // prints "int"
+        /// </code></example>
+        [PublicAPI, Pure]
+        public static string ReplaceWithBuiltInName(this string fullTypeName, bool withFolder = false)
+        {
+            const string builtInTypesPrefix = "Built-in.";
+
+            if (BuiltInTypes.TryGetValue(fullTypeName, out string builtInName))
+                return withFolder ? builtInTypesPrefix + builtInName : builtInName;
+            else
+                return fullTypeName;
         }
     }
 }
