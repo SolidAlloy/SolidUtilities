b287d0e6a2e680df108b3cb297e1eda7715bbc80
SolidAlloy <perepelartem@gmail.com>
2020-12-17 11:07:07 +0200

    feat: Added HashSet<T>.ExceptWithAndCreateNew() and IntersectWithAndCreateNew() methods

--- Runtime/Extensions/HashSetExtensions.cs
+++ Runtime/Extensions/HashSetExtensions.cs
@@ -13,7 +13,8 @@
         /// <param name="comparer">Comparer to use for the new set.</param>
         /// <typeparam name="T">Type of the HashSet collection elements.</typeparam>
         /// <returns>New set that contains only the elements located in <paramref name="thisSet"/> and not in <paramref name="otherSet"/>.</returns>
-        [PublicAPI, NotNull, Pure] public static HashSet<T> ExceptWith<T>(this HashSet<T> thisSet, HashSet<T> otherSet, EqualityComparer<T> comparer = null)
+        [PublicAPI, NotNull, Pure]
+        public static HashSet<T> ExceptWith<T>(this HashSet<T> thisSet, HashSet<T> otherSet, EqualityComparer<T> comparer = null)
         {
             var newSet = new HashSet<T>(thisSet, comparer ?? EqualityComparer<T>.Default);
             newSet.ExceptWith(otherSet);
@@ -28,9 +29,40 @@
         /// <param name="comparer">Comparer to use for the new set.</param>
         /// <typeparam name="T">Type of the HashSet collection elements.</typeparam>
         /// <returns>New set that contains elements located in both <paramref name="thisSet"/> and <paramref name="otherSet"/>.</returns>
-        [PublicAPI, NotNull, Pure] public static HashSet<T> IntersectWith<T>(this HashSet<T> thisSet, HashSet<T> otherSet, EqualityComparer<T> comparer = null)
+        [PublicAPI, NotNull, Pure]
+        public static HashSet<T> IntersectWith<T>(this HashSet<T> thisSet, HashSet<T> otherSet, EqualityComparer<T> comparer = null)
         {
             var newSet = new HashSet<T>(thisSet, comparer ?? EqualityComparer<T>.Default);
+            newSet.IntersectWith(otherSet);
+            return newSet;
+        }
+
+        /// <summary>
+        /// Generates a new set that contains only the elements located in <paramref name="thisSet"/> and not in <paramref name="otherSet"/>.
+        /// </summary>
+        /// <param name="thisSet">This set.</param>
+        /// <param name="otherSet">Other set.</param>
+        /// <typeparam name="T">Type of the HashSet collection elements.</typeparam>
+        /// <returns>New set that contains only the elements located in <paramref name="thisSet"/> and not in <paramref name="otherSet"/>.</returns>
+        [PublicAPI, NotNull, Pure]
+        public static HashSet<T> ExceptWithAndCreateNew<T>(this HashSet<T> thisSet, HashSet<T> otherSet)
+        {
+            var newSet = new HashSet<T>(thisSet);
+            newSet.ExceptWith(otherSet);
+            return newSet;
+        }
+
+        /// <summary>
+        /// Generates a new set that contains elements located in both <paramref name="thisSet"/> and <paramref name="otherSet"/>.
+        /// </summary>
+        /// <param name="thisSet">This set.</param>
+        /// <param name="otherSet">Other set.</param>
+        /// <typeparam name="T">Type of the HashSet collection elements.</typeparam>
+        /// <returns>New set that contains elements located in both <paramref name="thisSet"/> and <paramref name="otherSet"/>.</returns>
+        [PublicAPI, NotNull, Pure]
+        public static HashSet<T> IntersectWithAndCreateNew<T>(this HashSet<T> thisSet, HashSet<T> otherSet)
+        {
+            var newSet = new HashSet<T>(thisSet);
             newSet.IntersectWith(otherSet);
             return newSet;
         }
