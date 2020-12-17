f3317ed500ea97b1d971941cec1ebd1ea15932b0
SolidAlloy <perepelartem@gmail.com>
2020-12-17 11:05:56 +0200

    feat: Added AssetDatabaseHelper.GetTypeFromGUID() method

---
+++ Editor/Helpers/AssetDatabaseHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace SolidUtilities.Editor.Helpers
+{
+    using System;
+    using Extensions;
+    using JetBrains.Annotations;
+    using UnityEditor;
+
+    public static class AssetDatabaseHelper
+    {
+        /// <summary>
+        /// Retrieves type of the class located in an asset with the matching <paramref name="guid"/>.
+        /// </summary>
+        /// <param name="guid">The GUID of an asset to search for.</param>
+        /// <returns>Type of the class located in an asset with the matching <paramref name="guid"/>.</returns>
+        [PublicAPI, CanBeNull, Pure]
+        public static Type GetTypeFromGUID(string guid)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            var script = AssetDatabase.LoadAssetAtPath<MonoScript>(assetPath);
+
+            if (script == null)
+            {
+                return null;
+            }
+
+            var type = script.GetClassType();
+            return type;
+        }
+    }
+}
