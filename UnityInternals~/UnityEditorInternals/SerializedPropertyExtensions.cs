101a14a7f6e26874902b7cece18c5d44f3035deb
SolidAlloy <perepelartem@gmail.com>
2021-02-10 21:10:33 +0200

    feat: Added SerializedProperty.HasCustomPropertyDrawer() method

---
+++ UnityInternals~/UnityEditorInternals/SerializedPropertyExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SolidUtilities.UnityEditorInternals
+{
+    using JetBrains.Annotations;
+    using UnityEditor;
+
+    public static class SerializedPropertyExtensions
+    {
+        /// <summary> Checks if property has custom drawer. </summary>
+        /// <param name="property">The property to check.</param>
+        /// <returns> <c>true</c> if the <paramref name="property"/> has a custom drawer. </returns>
+        [PublicAPI]
+        public static bool HasCustomPropertyDrawer(this SerializedProperty property)
+        {
+            return ScriptAttributeUtility.GetHandler(property).propertyDrawer != null;
+        }
+    }
+}
