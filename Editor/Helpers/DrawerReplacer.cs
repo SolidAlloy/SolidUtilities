67c6f8ecfc7de42970b26429b9bcd0569af70f91
SolidAlloy <perepelartem@gmail.com>
2021-03-01 11:39:56 +0200

    feat: Added ReplaceDefaultDrawer method

---
+++ Editor/Helpers/DrawerReplacer.cs
@@ -0,0 +1,34 @@
+﻿namespace SolidUtilities.Editor.Helpers
+{
+    using JetBrains.Annotations;
+    using UnityEditor;
+
+    public static class DrawerReplacer
+    {
+        /// <summary>
+        /// Replaces the default <see cref="PropertyDrawer"/> for <typeparamref name="TObject"/> with a custom one.
+        /// </summary>
+        /// <typeparam name="TObject">
+        /// The type of object to use the custom <see cref="PropertyDrawer"/> for.
+        /// </typeparam>
+        /// <typeparam name="TDrawer">
+        /// The type of custom <see cref="PropertyDrawer"/> to use for <typeparamref name="TObject"/>.
+        /// </typeparam>
+        /// <example><code>
+        /// public class CustomUnityEventDrawer : PropertyDrawer
+        /// {
+        ///     [DidReloadScripts]
+        ///     private static void ReplaceDefaultDrawer()
+        ///         => DrawerReplacer.ReplaceDefaultDrawer&lt;UnityEventBase, CustomUnityEventDrawer>();
+        ///
+        ///     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) { }
+        /// }
+        /// </code></example>
+        [PublicAPI]
+        public static void ReplaceDefaultDrawer<TObject, TDrawer>()
+            where TDrawer : PropertyDrawer
+        {
+            UnityEditorInternals.DrawerReplacer.ReplaceDefaultDrawer<TObject, TDrawer>();
+        }
+    }
+}
---
+++ UnityInternals~/UnityEditorInternals/DrawerReplacer.cs
@@ -0,0 +1,26 @@
+﻿namespace SolidUtilities.UnityEditorInternals
+{
+    using JetBrains.Annotations;
+    using UnityEditor;
+
+    public static class DrawerReplacer
+    {
+        [PublicAPI]
+        public static void ReplaceDefaultDrawer<TObject, TDrawer>()
+            where TDrawer : PropertyDrawer
+        {
+            if (ScriptAttributeUtility.s_DrawerTypeForType == null)
+            {
+                ScriptAttributeUtility.BuildDrawerTypeForTypeDictionary();
+            }
+
+            var keySet = new ScriptAttributeUtility.DrawerKeySet
+            {
+                type = typeof(TObject),
+                drawer = typeof(TDrawer)
+            };
+
+            ScriptAttributeUtility.s_DrawerTypeForType[typeof(TObject)] = keySet;
+        }
+    }
+}
