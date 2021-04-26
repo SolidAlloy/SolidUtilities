9c95b8acb17634eeecd8c6403c25b845e482d1b6
SolidAlloy <perepelartem@gmail.com>
2021-04-26 22:05:06 +0300

    feat: Added Rect.ShiftLinesDown(int linesNum) method

---
+++ Editor/Extensions/RectExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SolidUtilities.Editor.Extensions
+{
+    using JetBrains.Annotations;
+    using UnityEditor;
+    using UnityEngine;
+
+    public static class RectExtensions
+    {
+        [PublicAPI]
+        public static Rect ShiftLinesDown(this Rect rect, int linesNum)
+        {
+            const float paddingBetweenFields = 2f;
+            const float indentPerLevel = 15f;
+
+            rect.xMin += linesNum * indentPerLevel;
+            rect.y += EditorGUIUtility.singleLineHeight + paddingBetweenFields;
+
+            return rect;
+        }
+    }
+}
