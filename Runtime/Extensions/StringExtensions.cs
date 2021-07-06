741fbbece90362b6ad986c6c608f355f2b3ad445
Artem Perepelitsa <perepelartem@gmail.com>
2021-07-06 22:53:48 +0300

    feat: Added string.IndexOfNth() extension method

--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace SolidUtilities.Extensions
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
@@ -139,6 +140,22 @@
             }
 
             return count;
+        }
+
+        [PublicAPI]
+        public static int IndexOfNth(this string str, char chr, int nth = 0)
+        {
+            if (nth < 0)
+                throw new ArgumentException("Can not find a negative index of substring in string. Must start with 0");
+
+            int offset = str.IndexOf(chr);
+            for (int i = 0; i < nth; i++)
+            {
+                if (offset == -1) return -1;
+                offset = str.IndexOf(chr, offset + 1);
+            }
+
+            return offset;
         }
     }
 }
