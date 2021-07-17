38d11e9c5043ff6c6301ab77d3fd349efeb3a9d9
Artem Perepelitsa <perepelartem@gmail.com>
2021-07-17 20:25:09 +0300

    feat: Added StackTraceHelper.EnvironmentToUnityStyle() and AddLinks() methods

---
+++ Runtime/Helpers/StackTraceHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace SolidUtilities.Helpers
+{
+    using System.Text.RegularExpressions;
+    using UnityEditorInternals;
+
+    public static class StackTraceHelper
+    {
+        public static string EnvironmentToUnityStyle(string stackTrace)
+        {
+            // Remove at at the start of lines
+            stackTrace = Regex.Replace(stackTrace, @" +at ", string.Empty);
+
+            // Remove the location part of the stack line if there is no reference to a file on disc
+            stackTrace = Regex.Replace(stackTrace, @" \[0x00000\] in <.*?(?=\n|$)", string.Empty);
+
+            // Replace [0x00000] in with (at
+            stackTrace = Regex.Replace(stackTrace, @"\[0x\w+?\] in", "(at");
+
+            // Add a closing parenthese
+            stackTrace = Regex.Replace(stackTrace, @"(?<=:\d+) ", ")");
+
+            return stackTrace;
+        }
+
+        public static string AddLinks(string unityStackTrace)
+        {
+            return ConsoleWindowProxy.StacktraceWithHyperlinks(unityStackTrace, 0);
+        }
+    }
+}
---
+++ UnityInternals~/UnityEditorInternals/ConsoleWindowProxy.cs
@@ -0,0 +1,12 @@
+﻿namespace SolidUtilities.UnityEditorInternals
+{
+    using UnityEditor;
+
+    public static class ConsoleWindowProxy
+    {
+        public static string StacktraceWithHyperlinks(string stackTrace, int startFrom)
+        {
+            return ConsoleWindow.StacktraceWithHyperlinks(stackTrace, startFrom);
+        }
+    }
+}
