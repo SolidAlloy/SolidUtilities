b8d29c0dc53896dc355953d51f83958a57b55bbb
Artem Perepelitsa <perepelartem@gmail.com>
2021-08-15 22:43:28 +0300

    feat: Added ListHelper.Empty<T>() method

---
+++ Runtime/Helpers/ListHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace SolidUtilities.Helpers
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public static class ListHelper
+    {
+        private static readonly Dictionary<Type, IList> _lists = new Dictionary<Type, IList>();
+
+        public static List<T> Empty<T>()
+        {
+            Type type = typeof(T);
+
+            if (_lists.TryGetValue(type, out IList list))
+                return (List<T>) list;
+
+            var newList = new List<T>();
+            _lists.Add(type, newList);
+            return newList;
+        }
+    }
+}
