6e8b360eab6cbadc8b8dd2d9488122876a42dd0a
Artem Perepelitsa <perepelartem@gmail.com>
2021-08-15 22:42:53 +0300

    feat: Added ICollection.AddIfMissing() extension method

---
+++ Runtime/Extensions/CollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace SolidUtilities.Extensions
+{
+    using System.Collections.Generic;
+
+    public static class CollectionExtensions
+    {
+        public static void AddIfMissing<T>(this ICollection<T> list, T value)
+        {
+            if ( ! list.Contains(value))
+                list.Add(value);
+        }
+    }
+}
