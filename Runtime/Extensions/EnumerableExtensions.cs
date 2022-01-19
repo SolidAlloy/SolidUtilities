02c02c94af6169ce4bc0d1080332650f5c01ea10
Artem <perepelartem@gmail.com>
2022-01-19 09:39:29 +0200

    feat: Added Enumerable.SelectWhere() extension method
    Sometimes, the same evaluation is used to filter the items and to select a value from them. For such cases, SelectWhere is better for performance reasons, so we don't run the same evaluation twice on items.

--- Runtime/Extensions/EnumerableExtensions.cs
+++ Runtime/Extensions/EnumerableExtensions.cs
@@ -14,5 +14,16 @@
         /// <returns>HashSet containing of the collection items.</returns>
         /// <exception cref="ArgumentNullException">If source is null.</exception>
         [PublicAPI, NotNull] public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source) => new HashSet<T>(source);
+
+        public static IEnumerable<TResult> SelectWhere<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, (bool eligible, TResult result)> predicate)
+        {
+            foreach (TSource element in source)
+            {
+                (bool eligible, TResult result) = predicate(element);
+
+                if (eligible)
+                    yield return result;
+            }
+        }
     }
 }
