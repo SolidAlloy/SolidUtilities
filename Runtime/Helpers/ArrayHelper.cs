cebdd0f601895394c0dd7c015792ea9905fc17a3
Artem <perepelartem@gmail.com>
2022-01-23 00:01:13 +0200

    feat: Added the ArrayHelper class with methods that allow changing the size of the array
    The methods repeat the functionality of ArrayUtility in UnityEditor but are located in the runtime library and are more efficient than some ArrayUtility methods.

---
+++ Runtime/Helpers/ArrayHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace SolidUtilities
+{
+    using System;
+
+    public static class ArrayHelper
+    {
+        public static void Add<T>(ref T[] array, T item)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = item;
+        }
+        
+        public static void RemoveAt<T>(ref T[] array, int index)
+        {
+            if (index < 0 || index >= array.Length)
+                throw new ArgumentOutOfRangeException($"The passed index {index} is out of bounds of the array with length {array.Length}");
+
+            var newArray = new T[array.Length - 1];
+
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
+                    continue;
+
+                newArray[j] = array[i];
+                j++;
+            }
+
+            array = newArray;
+        }
+
+        public static bool Remove<T>(ref T[] array, T item)
+        {
+            int index = Array.IndexOf(array, item);
+
+            if (index == -1)
+                return false;
+            
+            RemoveAt(ref array, index);
+            return true;
+        }
+    }
+}
