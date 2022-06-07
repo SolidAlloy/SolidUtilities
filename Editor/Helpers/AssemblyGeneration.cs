5f7dea715a639b2f52badcaeafa81a652d6412bc
Artem <perepelartem@gmail.com>
2022-06-07 20:31:38 +0300

    feat: Added an option to exclude assembly from editor during import

--- Editor/Helpers/AssemblyGeneration.cs
+++ Editor/Helpers/AssemblyGeneration.cs
@@ -9,11 +9,11 @@
     /// </summary>
     public static class AssemblyGeneration
     {
-        public static void ImportAssemblyAsset(string assemblyPath, string assemblyGUID, bool editorOnly = false)
+        public static void ImportAssemblyAsset(string assemblyPath, string assemblyGUID, bool editorOnly = false, bool excludeEditor = false)
         {
             // Sometimes, we need to know GUID of a future DLL before we start creating and importing it.
             // That's why it's passed as a parameter.
-            string metaContent = editorOnly ? GetEditorMetaContent(assemblyGUID) : GetMetaFileContent(assemblyGUID);
+            string metaContent = editorOnly ? GetEditorMetaContent(assemblyGUID) : GetMetaFileContent(assemblyGUID, excludeEditor);
 
             // Starting from Unity 2019, .meta files are hidden and their direct editing via File.WriteAllText can
             // raise exceptions. Operating on a file through FileStream is safer and allows to be sure the file will be
@@ -31,7 +31,7 @@
             AssetDatabase.ImportAsset(assemblyPath);
         }
 
-        private static string GetMetaFileContent(string guid)
+        private static string GetMetaFileContent(string guid, bool excludeEditor)
         {
             // Auto Reference is disabled, Validate References is disabled, all platforms are enabled, and Any CPU/OS is chosen for all platforms.
             return $@"fileFormatVersion: 2
@@ -52,7 +52,7 @@ PluginImporter:
     second:
       enabled: 0
       settings:
-        Exclude Editor: 0
+        Exclude Editor: {(excludeEditor ? "1" : "0")}
         Exclude Linux64: 0
         Exclude OSXUniversal: 0
         Exclude Win: 0
