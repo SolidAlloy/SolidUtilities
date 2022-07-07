0965d89bf71c0d6ee97f4c9d1ed31e658b6c0ccf
SolidAlloy <perepelartem@gmail.com>
2022-07-07 14:58:49 +0300

    feat: Added SerializedObject.SetHideFlagsPersistently() extension method

---
+++ Editor/Extensions/SerializedObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SolidUtilities.Editor
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    public static class SerializedObjectExtensions
+    {
+        public static void SetHideFlagsPersistently(this SerializedObject serializedObject, HideFlags flags)
+        {
+            // The only way to set the hide flags persistently.
+            var hideFlagsProp = serializedObject.FindProperty("m_ObjectHideFlags");
+            int flagsValue = (int) flags;
+
+            if (hideFlagsProp.intValue != flagsValue)
+            {
+                hideFlagsProp.intValue = flagsValue;
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
