38c5068e9df3355211d48f08feb3efcb3522bf9c
SolidAlloy <perepelartem@gmail.com>
2022-07-24 23:46:14 +0300

    fix: Fixed exception when passing null array to ArrayHelper.Add()

--- Runtime/Helpers/ArrayHelper.cs
+++ Runtime/Helpers/ArrayHelper.cs
@@ -6,10 +6,16 @@
     {
         public static void Add<T>(ref T[] array, T item)
         {
+            if (array == null)
+            {
+                array = new T[] { item };
+                return;
+            }
+
             Array.Resize(ref array, array.Length + 1);
             array[array.Length - 1] = item;
         }
-        
+
         public static void RemoveAt<T>(ref T[] array, int index)
         {
             if (index < 0 || index >= array.Length)
@@ -36,7 +42,7 @@
 
             if (index == -1)
                 return false;
-            
+
             RemoveAt(ref array, index);
             return true;
         }
